1801c99987e3e7a2bc3a0335d8e61752b2b672d6
s35792-cmd <s35792@pjwstk.edu.pl>
2026-03-24 13:35:20 +0100

    Add files via upload

--- main.java
+++ main.java
@@ -9,5 +9,38 @@ public class main {
         b1.borrow();
         b2.borrow();
         b1.returnBook();
+
+        Reader r1 = new Reader("John", "Hudson", 243, 0);
+        Reader r2 = new Reader("Marry", "Connor", 638, 3);
+        r1.printData();
+        r1.increaseBorrowedCount();
+        r1.printData();
+
+        r2.printData();
+        r2.decreaseBorrowedCount();
+        r2.printData();
+
+        Library library = new Library(5);
+        library.addBook(b1);
+        library.addBook(b2);
+        library.addBook(b3);
+        library.printAvailableBooks();
+        Book found = library.findBookByTitle("The 22 laws of marketing");
+        if(found!=null){
+            found.printinfo();
+        }
+        System.out.println(library.countAvailableBooks());
+        b2.printinfo();
+        r1.printData();
+        if(b2.isAvailable()){
+            b2.borrow();
+            r1.increaseBorrowedCount();
+        }
+        b2.printinfo();
+        r1.printData();
+        b2.returnBook();
+        r1.decreaseBorrowedCount();
+        b2.printinfo();
+        r1.printData();
     }
 }
